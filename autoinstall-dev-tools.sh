a4fbc4388cbc29a8d1cc7bb790638ba79cd23b07
Enrico Alletto <enrico.alletto@gmail.com>
2018-11-04 20:38:35 +0100

    comandi affinati

--- autoinstall-dev-tools.sh
+++ autoinstall-dev-tools.sh
@@ -7,25 +7,50 @@
 # Note:         for help autoinstall-dev-tools.sh help
 # ************************************************************
 
+#########################
+# The command line help #
+#########################
+display_help() {
+    echo
+    echo "Usage: $0 [option...] {list|verify|install|remove}" >&2
+    echo
+    echo "   list           ottieni la lista di tutti i pacchetti installati"
+    echo "   verify         verifica se sono installati i pacchetti development"
+    echo "   install        installa i pacchetti development"
+    echo "   remove         rimuovi i pacchetti development"
+    echo
+    # echo some stuff here for the -a or --add-options
+    exit 1
+}
+
 case $1 in
   list)
-    dpkg -l | grep ^ii | awk '{print $2, $3}'
+    dpkg -l
+    echo
     dpkg -l | grep ^ii | wc -l
-
+    echo
+    # dpkg -l | grep ^ii | awk '{print $2, $3, $4, $5}'
+    echo Comando da lanciare per filtrare pacchetti specifici: "dpkg -l | grep <pacchetto>"
+    echo
   ;;
   verify)
     # deve essere presente il file dev-pack.txt
     dpkg -l | grep ^ii | awk '{print $2}' > package.txt # crea file per confronare i pacchetti installati
     echo
-    echo OK package
+    echo "**********************"
+    echo "* Packages installed *"
+    echo "**********************"
     diff dev-pack.txt package.txt -u | grep -v "^\+" | grep -v "^\-" | grep -v "^\@@"
     echo
-    echo Install package
+    echo "**********************"
+    echo "* Package to install *"
+    echo "**********************"
     diff dev-pack.txt package.txt -u | grep -v "^\+" | grep -e "^\-" | grep -v "^\---"
     echo
     rm package.txt
   ;;
   install)
+    sudo ls # chiede la password sudo
     read -r -p "Vuoi proseguire a installare i pacchetti? [S/n]" response
     response=${response,,} # tolower
       if [[ $response =~ ^(si|s| ) ]] || [[ -z $response ]]; then
@@ -48,6 +73,6 @@ case $1 in
     echo removing
   ;;
   *)
-  echo ATTENZIONE! Non hai inserito il comando corretto. Controlla help
+  display_help
   ;;
 esac
